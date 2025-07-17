6f04d7f057f9b3a0917126f7b0b4a731c2721c6d
Nada-ibrahim12 <123187920+Nada-ibrahim12@users.noreply.github.com>
2025-07-18 01:18:44 +0300

    leetcode problem Replace Employee ID With The Unique Identifier

--- leetcode/SQL 50/06- Replace Employee ID With The Unique Identifier.sql
+++ leetcode/SQL 50/06- Replace Employee ID With The Unique Identifier.sql
@@ -1,1 +1,5 @@
--- problem URL: https: // leetcode.com / problems / replace - employee - id - with - the - unique - identifier / ? envType = study - plan - v2 & envId = top - sql -50
+--problem URL: https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/?envType=study-plan-v2&envId=top-sql-50
+select EmployeeUNI.unique_id,
+    Employees.name
+from EmployeeUNI
+    right join Employees on EmployeeUNI.id = Employees.id
